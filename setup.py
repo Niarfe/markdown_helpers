e029b5cc4d7db8d4d68bf471d342a55bdf6d867f
Niarfe <efrain.olivares@gmail.com>
2024-05-29 20:40:03 -0700

    add the exe

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
       name='markdown_helpers',
       version='0.0.2',
+      scripts='mdformat'],
       description='Markdown formatting helpers',
       author='Efrain Olivares',
       author_email='efrain.olivares@gmail.com',
